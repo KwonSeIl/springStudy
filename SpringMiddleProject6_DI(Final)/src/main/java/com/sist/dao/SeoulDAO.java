60a7eee201d6555767c55059025260ec1193cbb9
KwonSeIl <seilzoa@naver.com>
2023-02-19 15:51:24 +0900

    springMiddleProject6

---
+++ SpringMiddleProject6_DI(Final)/src/main/java/com/sist/dao/SeoulDAO.java
@@ -0,0 +1,17 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.SeoulMapper;
+import com.sist.vo.SeoulVO;
+@Repository
+public class SeoulDAO {
+	@Autowired
+	private SeoulMapper mapper;
+	public List<SeoulVO> seoulListData(Map map)
+	{
+		return mapper.seoulListData(map);
+	}
+}
